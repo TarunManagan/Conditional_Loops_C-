c8c236b8c8d66cfcf1bc8d43bb9656056bc48578
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 14:43:10 +0530

    Create HelloWorld_FOR_loop.cpp

---
+++ HelloWorld_FOR_loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<=10;i++){// for(Initialization; Condition; Incrementation/Decrementation)
+       cout<<"Hello World!!"<<"\n";
+    }
+    return 0;
+}
+/*OUTPUT
+Hello World!!
+Hello World!!
+Hello World!!
+Hello World!!
+Hello World!!
+Hello World!!
+Hello World!!
+Hello World!!
+Hello World!!
+Hello World!!
+*/
